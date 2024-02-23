06be16e79ce6b04817e64e0d35c8737a5a807ab9
dannygorgon <thirdoptionisthename@googlemail.com>
2024-02-23 11:27:38 +0000

    add WIP responsive hero section

--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -3,9 +3,17 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section>
-    <h1 className="text-3xl xs:text-4xl sm:text-5xl font-bold text-center">Hello, I&apos;m <span className="text-blue-500">Daniel</span></h1>
-    <Image src="/me.png" alt="Hero" width={170} height={177} className="m-3 mx-auto" />    <p className="text-3xl font-bold text-center">I&apos;m a <span className="text-blue-500">full-stack developer</span></p>
+    <section className="flex flex-col sm:flex-row items-center justify-center w-full mx-auto space-x-8 sm:space-x-16 lg:space-x-32">
+      <div className="text-left">
+        <h1 className="text-3xl xs:text-4xl sm:text-5xl font-bold">Hello, I&apos;m <span className="text-blue-500">Daniel</span></h1>
+        
+        <p className="text-xl xs:text-2xl font-bold ">I&apos;m a <span className="text-blue-500">full-stack developer</span></p>
+        <p className="text-xl xs:text-2xl font-bold ">I&apos;m a <span className="text-blue-500">full-stack developer</span></p>
+
+      </div>
+      <div>
+        <Image src="/me.png" alt="Hero" width={170} height={177}  />
+      </div>
     </section>
   );
 };
