8a459bd39aa7e1d86d3d4b75e1283153aa308100
dannygorgon <thirdoptionisthename@googlemail.com>
2024-02-28 03:00:47 +0000

    remove top padding from layout

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <header className="w-full mx-auto max-w-screen-xl px-8 sm:px-16">
         <Nav /> 
       </header>
-      <main className="flex min-h-screen flex-col items-center justify-between p-8">
+      <main className="flex min-h-screen flex-col items-center justify-between px-8">
         {children}
       </main>
     </>
