4ccc878a51b3d3f1ba20c48194826a17172edccb
dannygorgon <thirdoptionisthename@googlemail.com>
2024-03-08 11:56:48 +0000

    remove spacing in hero section, add more width classes to image

--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="flex flex-col sm:flex-row items-center justify-center w-full mx-auto max-w-screen-xl space-x-4 sm:space-x-8 lg:space-x-16 xl:space-x-24 2xl:space-x-64">
+<section className="flex flex-col sm:flex-row items-center justify-center w-full mx-auto max-w-screen-xl">
       <div className="text-left">
         <h1 className="text-3xl xs:text-4xl sm:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl font-bold text-balance">Hello, I&apos;m <span className="text-blue-500">Daniel</span></h1>
         <p className="text-xl xs:text-2xl sm:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold text-balance">I&apos;m a <span className="text-blue-500">full-stack developer</span></p>
@@ -16,8 +16,9 @@ const Hero = () => {
   alt="Hero" 
   width={170} 
   height={177} 
-  className="transition-all duration-500 ease-in-out transform xl:w-96 2xl:w-128" 
-/>      </div>
+
+
+  className="transition-all duration-500 ease-in-out transform w-64 md:w-72 lg:w-80 xl:w-96 2xl:w-128"/>      </div>
     </section>
   );
 };
